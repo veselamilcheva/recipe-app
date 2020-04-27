9ac82a65574494d030095929f6a20bbe10a84181
Vesela Milcheva <milchevavesela@gmail.com>
2020-04-27 18:31:18 -0400

    emit event from header(child) to app(parent) and passing data from recipe-list(parent) to recipe-item(child)

--- recipe-app/src/app/app.component.ts
+++ recipe-app/src/app/app.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'recipe-app';
+  loadedPage = 'recipe';
+
+  onNavigate(page: string) {
+    this.loadedPage = page;
+  }
 }
--- recipe-app/src/app/header/header.component.ts
+++ recipe-app/src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -6,11 +6,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
+  @Output() showPage = new EventEmitter<string>();
   isCollapsed = false;
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onSelect(page: string) {
+    this.showPage.emit(page)
   }
 
 }
